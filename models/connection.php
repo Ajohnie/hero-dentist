67196e175c9032db40638c340ef767099cfb53e9
Ajohnie <jakankwasa.tech@yahoo.com>
2021-01-06 19:44:02 +0300

    Removed private firebase config

--- models/connection.php
+++ models/connection.php
@@ -7,17 +7,17 @@ use Kreait\Firebase\Factory;
 use Kreait\Firebase\ServiceAccount;
 
 define('FIREBASE_CONFIG', array(
-    'databaseURL' => 'https://dental-hero-1e694.firebaseio.com/',
+    'databaseURL' => '',
     "type" => "service_account",
-    "project_id" => "dental-hero-1e694",
-    "private_key_id" => "802b6ea6ac8c5fde3e4d755c919d4fd947614742",
-    "private_key" => "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDACJJ/ZBibIsKG\nznE1EU8rTQYfMipcgACQLKIwmaKmYVuviZkjxG1Yq0/4vxpI8ZISN8Yo7VgRH0rI\nPeaCz2NsPCmlngTTJPFs/qC7utRStw0EoeiXJKPyQs8QjKQe7PkFmlCUUluxkSdZ\nmN3CBOrKtrqFobummGXb9P+8NZ1GNrmB1/EaFM5RRKL1v5LLbRLR+6IIBcnJLfV1\n8SGm7XaBHC3Ph+THlNXC0ZBK7r17OEcSKBTvSw3cb+5LK7K3r5HPGYJEgyOpl1pz\n1AY2Jo4QW+4SXNa8jerkO2roF0RiHsrOK95x0wiiFRoJmextB4HZOMYoFZI0MUX/\n1Zotoe9tAgMBAAECggEAIRwpRDE6Q99fqnYkU0nPfHAMnurbnnTMhA9KDLaksmrF\nb/P51U6sKtZo1MMAcXTDYoQ+1LYtkdAzKOjxWnsPEa2i4aUoAE7VmJ0jkhLBwpxA\nsJyI+axt3xqxUJFrR9DEk/h6LcSwfiKApiRjEe2enQKaEOnkrLIIl/77bBRTcHQ4\nqpFF613YY1fwKWhuSx8pEjUt7Ervalv5L3XPnCUgjYTiVLh58boqX7PNDUz5Kv/d\nDFYAqtjaI9sacUZixloVbQANU4Muz7QPQnU1iwI3t5NyZPFOD18oW342iG1RhfX7\nRjgUv3iqUg9YVTsjCEn5+/MmvItFZ9VoqIlSYYxZeQKBgQD3COCUtWBaScacDh3L\n1WAniX63ZEiaRWGy2K/hQEYAO+iHodPF8tsLE1TvBQO2tcHZFfCtK39uyca8wR/L\nlp/OgQyUs1Xt1CYvXdQOBxiIAxeHi3ASDWtACIHMobYxBafT7Ra1jMKvW+7+IilM\nJ3ONvJ7FIvLDLW3y3q3fJZgPxQKBgQDHALIpDw5SQdV0NBYHVc3uysSEEWwqUH3d\nr6U9TFcZUseHoBm/qZfVsAspFBNgWe2eUlQNj2puHZefiKg2YjF8G8XoSAFWJJSH\nTG4byCMJe64Zi+7QGTJ/kmHj8sXe2Grzoo0r8NRienNgp90vQ0isxv1SJnHh+x3d\n+WPKtjxziQKBgQCYXsJfFwyqRA0b3l0kh6T6XgVpypI5jYf4T00Ui5dht5Hybq93\nzharWy4tC5PB05QQeUbtGQJN7GkHrbsV0P8a2WBQK9lZi1xOJPqR+HB6HCYcNbZX\nfC1l3plpBWmiDRH6VqeGLZ87mGHuIFYGoeLVTr1DbQ39svy5K/5kwARt3QKBgFB6\nr5J7TEdn6HM+PUpqIQZZanAeu6fBNaXZyvGnVfIf0CHBVah1Zbo9n8XVIKDuvXIx\nyVxSi7BMt20FT0HvJCQU90yhEilMUeUFu+HwZnsHUblzJflyt0FwabZOEEe3Lkmp\ni34hv0K0UsTcHk9m3pVBRGFcuuGxNLr4iKsKWBhBAoGAdH5kCUo45KaarN7FQ6I/\nVvs8ksubKNXKAWHcv2zGKIv+qVf0L9UPZPM7rzjZYm2D/of/p5GkTgQN8XnvuvbD\nLfIjl6AJqAu45CgurodXQWt9b/YmjBISiKoK3nS5MA3D/HdXqEzcsR4iS/+lgPak\nEX/Pe8WVF6CL8Oap59Rswsc=\n-----END PRIVATE KEY-----\n",
-    "client_email" => "dental-hero-1e694@appspot.gserviceaccount.com",
-    "client_id" => "101395232055487550918",
+    "project_id" => "",
+    "private_key_id" => "",
+    "private_key" => "",
+    "client_email" => "",
+    "client_id" => "",
     "auth_uri" => "https://accounts.google.com/o/oauth2/auth",
     "token_uri" => "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url" => "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url" => "https://www.googleapis.com/robot/v1/metadata/x509/dental-hero-1e694%40appspot.gserviceaccount.com"
+    "client_x509_cert_url" => ""
 ));
 
 define('USERS_COLLECTION', 'Admin');
